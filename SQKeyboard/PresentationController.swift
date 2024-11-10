470daf386f1df8deda69e63d8ad6fb5504d8f993
luxingkai <18356285335@163.com>
2024-11-10 21:44:31 +0800

    complete view controller transition delegate and custom PresentationController

---
+++ SQKeyboard/PresentationController.swift
@@ -0,0 +1,12 @@
+//
+//  PresentationController.swift
+//  SQKeyboard
+//
+//  Created by luxingkai on 2024/11/9.
+//
+
+import UIKit
+
+class PresentationController: UIPresentationController {
+
+}
